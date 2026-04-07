aefdd9c39b503903485c7d936af94e48084bbcc2
Gabriel Mancin <gabriel.mancin@agenciafg.com.br>
2026-04-07 11:35:42 -0300

    insercao de icone de aviso pdp

--- app-custom/react/components/cashback/index.tsx
+++ app-custom/react/components/cashback/index.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { useProduct } from "vtex.product-context";
+/* importa o css do mesmo diretório */
+import "./styles.css";
 
 export const ProductCashbackDisplay: React.FC = () => {
   const product = useProduct();
 
   // Pega o preço de venda do produto
-  const sellingPrice = product?.selectedItem?.sellers[0].commertialOffer.Price;
+  const sellingPrice =
+    product?.selectedItem?.sellers[0].commertialOffer.Price;
 
   // Calcula 30% do preço de venda
   const cashbackAmount = sellingPrice ? sellingPrice * 0.3 : 0;
@@ -17,11 +20,30 @@ export const ProductCashbackDisplay: React.FC = () => {
   });
 
   return (
-    <div className="vtex-rich-text-0-x-container vtex-rich-text-0-x-container--cashback__pdp flex tl items-start justify-start t-body c-on-base">
-      <div className="vtex-rich-text-0-x-wrapper vtex-rich-text-0-x-wrapper--cashback__pdp">
-        <p className="lh-copy vtex-rich-text-0-x-paragraph vtex-rich-text-0-x-paragraph--cashback__pdp">
-          Compre e acumule bônus de <span className="b vtex-rich-text-0-x-strong vtex-rich-text-0-x-strong--cashback__pdp">{cashbackFormatted}</span>
+    <div className="vtex-rich-text-0-x-container vtex-rich-text-0-x-container--cashback__pdp flex tl items-center justify-start t-body c-on-base">
+      <div className="vtex-rich-text-0-x-wrapper vtex-rich-text-0-x-wrapper--cashback__pdp flex items-center">
+
+        <p className="lh-copy vtex-rich-text-0-x-paragraph vtex-rich-text-0-x-paragraph--cashback__pdp flex items-center">
+          Compre e acumule bônus de{" "}
+
+          <span className="b vtex-rich-text-0-x-strong vtex-rich-text-0-x-strong--cashback__pdp">
+            {cashbackFormatted}
+          </span>
+
+          {/* Ícone de informação */}
+          <span className="cashback-info-wrapper">
+
+            {/* círculo com "i" */}
+            <span className="cashback-info-icon">i</span>
+
+            {/* tooltip exibido no hover */}
+            <span className="cashback-tooltip">
+              Bônus disponibilizado em 5 dias úteis após a entrega
+            </span>
+
+          </span>
         </p>
+
       </div>
     </div>
   );
